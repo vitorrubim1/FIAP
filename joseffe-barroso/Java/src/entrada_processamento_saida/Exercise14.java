4eea9b761b5b1cecfd72ac5a01deca1bd17e98f1
Vitor Rubim <48107882+vitorrubim1@users.noreply.github.com>
2022-08-30 22:07:14 -0300

    feat: add exercise 14 java

--- joseffe-barroso/Java/src/entrada_processamento_saida/Exercise14.java
+++ joseffe-barroso/Java/src/entrada_processamento_saida/Exercise14.java
@@ -1,10 +1,31 @@
 package entrada_processamento_saida;
 
+import java.util.Scanner;
+
 public class Exercise14 {
 
 	public static void main(String[] args) {
-    // Triangulo rentagulo
+		Scanner scanner = new Scanner(System.in);
+		
+		double heigth, catetoOne, catetoTwo, catetoSum;
+		
+		System.out.print("Digite a altura: ");
+		heigth = scanner.nextDouble();
+		
+		System.out.print("Digite o primeiro cateto: ");
+		catetoOne = scanner.nextDouble();
+		
+		System.out.print("Digite o primeiro cateto: ");
+		catetoTwo = scanner.nextDouble();
+		
+		catetoSum = (catetoOne * catetoOne) + (catetoTwo * catetoTwo);
+		
+		if ((heigth * heigth) == catetoSum) {
+			System.out.println("É um triângulo retângulo");
+		} else {
+			System.out.println("Não é um triângulo retângulo");
+		}
 
+		scanner.close();
 	}
-
 }
