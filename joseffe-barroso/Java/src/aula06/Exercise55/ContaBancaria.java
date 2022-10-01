f81c778543cc6a4972ec71a1a24db7e3b9c68ff6
Vitor Rubim <vitorrubim1@hotmail.com>
2022-09-30 21:07:46 -0300

    refactor: added value to atributte

--- joseffe-barroso/Java/src/aula06/Exercise55/ContaBancaria.java
+++ joseffe-barroso/Java/src/aula06/Exercise55/ContaBancaria.java
@@ -10,6 +10,8 @@ public class ContaBancaria {
 	}
 	
 	public ContaBancaria(String agencia, String numero , double saldo) {
-		
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
 	}
 }
