5dac340e176475e60922d2e9935714d5cb8c0bda
Vitor Rubim <vitorrubim1@hotmail.com>
2022-10-19 20:45:26 -0300

    refactor: change function validate form fields

--- alexander-gobatto/htmlAndCss/exercise05/script.js
+++ alexander-gobatto/htmlAndCss/exercise05/script.js
@@ -1,33 +1,28 @@
-function submit() {
-    const fullname = document.getElementsByName("fullname");
-    const email = document.getElementsByName("email");
-    const password = document.getElementsByName("password");
+function validateRequiredInputs(inputs) {
+    inputs.forEach(inputName => {
+        const input = document.getElementById(inputName);
+        const inputIsEmpty = !input.value.trim();
 
-    const fields = [fullname, email, password];
-    
-    // Span error element
-    fields.forEach(item => {
-        const inputIsEmpty = !item[0].value.trim();
-        const inputName = item[0].name;
-        
         if(inputIsEmpty) {
             const newSpanError = document.createElement("span");
-            const newSpanErrorContent = document.createTextNode("Este campo é obrigatório.");
+            const newSpanErrorContent = document.createTextNode(`O campo ${input.name} é obrigatório.`);
             newSpanError.appendChild(newSpanErrorContent);
             
-            const currentInput = item[0];
-            newSpanError.classList.add("error")
-            newSpanError.setAttribute("id", `error-${inputName}`)
+            newSpanError.classList.add("error");
+            input.classList.add("inputWithError");
+            newSpanError.setAttribute("id", `error-${inputName}`);
 
             const spanErrorAlreadyExist = document.getElementById(`error-${inputName}`);
 
             if (spanErrorAlreadyExist) return;
 
-            document.body.insertBefore(newSpanError, currentInput.nextSibling)
+            document.body.insertBefore(newSpanError, input.nextSibling)
         } else {
             const spanError = document.getElementById(`error-${inputName}`);
+            const inputWithError = input.classList.remove("inputWithError");
 
             if (!!spanError) spanError.remove();
+            if (!!inputWithError) input.classList.remove("inputWithError")
         }
     })
 }
