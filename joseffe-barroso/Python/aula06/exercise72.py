9d9e78b76cc3858dc329b19b3cc1fe10f0df56bf
Vitor Rubim <vitorrubim1@hotmail.com>
2022-11-04 22:24:05 -0300

    refactor: python exercise 72

--- joseffe-barroso/Python/aula06/exercise72.py
+++ joseffe-barroso/Python/aula06/exercise72.py
@@ -1,91 +1,89 @@
-manha = []
-linhasManha=0
-colunasManha=0
-
-tarde = []
-linhasTarde=0
-colunasTarde=0
-
-noite = []
-linhasNoite=0
-colunasNoite=0
-
-
-retornar = "s"
-empty = "-----";
-lotacao=0
-
-for index in range(0, 3, 1):
-    if (index == 0):
-        print("SESSÃO MATINAL")
-        linhasManha = int(input('Digite a quantidade de fileiras que a casa de show terá: '))
-        colunasManha = int(input('Digite a quantidade de poltronas por fileira que a casa de show terá: '))
-    if (index == 1):
-        print("\nSESSÃO VESPERTINA")
-        linhasTarde = int(input('Digite a quantidade de fileiras que a casa de show terá: '))
-        colunasTarde = int(input('Digite a quantidade de poltronas por fileira que a casa de show terá: '))
-    if (index == 2):
-        print("\nSESSÃO NOTURNA")
-        linhasNoite = int(input('Digite a quantidade de fileiras que a casa de show terá: '))
-        colunasNoite = int(input('Digite a quantidade de poltronas por fileira que a casa de show terá: '))
-
-
-for index in range(0, linhasManha, 1):
+manha=[]
+tarde=[]
+noite=[]
+lotacao_manha = 0
+lotacao_tarde = 0
+lotacao_noite = 0
+retornar="s"
+ 
+print("Reservas do Show do CBJR \o/")
+ 
+fileiras = int(input('Quantas fileiras a casa de show terá? '))
+cadeiras = int(input('Quantas cadeiras cada fileira da casa de show terá? '))
+ 
+for i in range(0, fileiras, 1):
     manha.append([])
-
-for index in range(0, linhasTarde, 1):
     tarde.append([])
-    
-for index in range(0, linhasNoite, 1):
     noite.append([])
-
-# Populando a matriz MANHÃ
-for line in range(0, linhasManha, 1):
-    for column in range(0, colunasManha, 1):
-        manha[line].append(empty)
-
-# Populando a matriz TARDE
-for line in range(0, linhasTarde, 1):
-    for column in range(0, colunasTarde, 1):
-        manha[line].append(empty)
-
-# Populando a matriz NOITE
-for line in range(0, linhasNoite, 1):
-    for column in range(0, colunasNoite, 1):
-        manha[line].append(empty)
-
-while(retornar == "s"): 
-    name = input("Digite o nome do festeiro: ")
-
-    fileira = int(input("Digite a fileira que você deseja sentar (1 a 10): ")) - 1  # Linha
-    cadeira = int(input("Digite a cadeira que você deseja sentar (1 a 4): ")) - 1   # Coluna
-
-    while (manha[fileira][cadeira] != empty):
-        print("Essa assento já está reservado.")
-        fileira = int(input("Digite novamente a fileira que você deseja sentar (1 a 10): ")) - 1  # Linha
-        cadeira = int(input("Digite a cadeira que você deseja sentar (1 a 4): ")) - 1   # Coluna
-  
-    manha[fileira][cadeira] = name
-    print("Reserva realizada com sucesso!")
-    lotacao = lotacao + 1
-
-    if (lotacao != 40):
-        retornar = input("Deseja realizar mais alguma reservar? S/N: ").lower()
-        while (retornar != "n" and retornar != "s"):
-            print("Digite somente S ou N")
-            retornar = input("Deseja realizar mais alguma reservar? S/N: ").lower()
+ 
+for l in range(0, fileiras, 1):
+    for c in range(0, cadeiras, 1):    
+        manha[l].append("vazio")
+        tarde[l].append("vazio")
+        noite[l].append("vazio")
+ 
+while(retornar=="s"):
+    nome = input("Digite o seu nome: ")
+    sessao = int(input("Digite a sessão que você deseja fazer a reserva (1-Manha, 2-Tarde, 3-Noite): "))
+    fileira = int(input("Digite a fileira que você deseja sentar (1 até " + str(fileiras) + "): ")) - 1
+    cadeira = int(input("Digite a cadeira que você deseja sentar (1 até " + str(cadeiras) + "): ")) - 1
+ 
+    while((sessao == 1) and (lotacao_manha >= ((fileiras * cadeiras)*0.80) ) ):
+        print('Sessao lotada, escolha outra sessão!')
+        sessao = int(input("Digite a sessão que você deseja fazer a reserva (1-Manha, 2-Tarde, 3-Noite): "))
+ 
+    while((sessao == 2) and (lotacao_tarde >= ((fileiras * cadeiras)*0.80) ) ):
+        print('Sessao lotada, escolha outra sessão!')
+        sessao = int(input("Digite a sessão que você deseja fazer a reserva (1-Manha, 2-Tarde, 3-Noite): "))
+ 
+    while((sessao == 3) and (lotacao_noite >= ((fileiras * cadeiras)*0.80) ) ):
+        print('Sessao lotada, escolha outra sessão!')
+        sessao = int(input("Digite a sessão que você deseja fazer a reserva (1-Manha, 2-Tarde, 3-Noite): "))
+ 
+    if (sessao == 1):
+        while (manha[fileira][cadeira] != "vazio"):
+            print("Esse assento já está reservado!")
+            fileira = int(input("Digite a fileira que você deseja sentar (1 até " + str(fileiras) + "): ")) - 1
+            cadeira = int(input("Digite a cadeira que você deseja sentar (1 até " + str(cadeiras) + "): ")) - 1
+ 
+        manha[fileira][cadeira] = nome
+        lotacao_manha = lotacao_manha + 1
+    elif (sessao == 2):
+        while (tarde[fileira][cadeira] != "vazio"):
+            print("Esse assento já está reservado!")
+            fileira = int(input("Digite a fileira que você deseja sentar (1 até " + str(fileiras) + "): ")) - 1
+            cadeira = int(input("Digite a cadeira que você deseja sentar (1 até " + str(cadeiras) + "): ")) - 1
+ 
+        tarde[fileira][cadeira] = nome
+        lotacao_tarde = lotacao_tarde + 1
     else:
-        print("O ônibus está lotado")
+        while (noite[fileira][cadeira] != "vazio"):
+            print("Esse assento já está reservado!")
+            fileira = int(input("Digite a fileira que você deseja sentar (1 até " + str(fileiras) + "): ")) - 1
+            cadeira = int(input("Digite a cadeira que você deseja sentar (1 até " + str(cadeiras) + "): ")) - 1
+        noite[fileira][cadeira] = nome
+        lotacao_noite = lotacao_noite + 1
+ 
+    print("Reserva realizada com sucesso!")
+   
+    if ( (lotacao_manha >= ((fileiras * cadeiras)*0.80) ) and
+         (lotacao_tarde >= ((fileiras * cadeiras)*0.80) ) and
+         (lotacao_noite >= ((fileiras * cadeiras)*0.80) ) ):
+        print("Todas as sessões estão lotadas!")
         break
-
-print("Mapa da reserva da casa de show MANHÃ: ")
-for index in range(0, linhas, 1):
-    print(manha[index]);
-
-print("Mapa da reserva da casa de show TARDE: ")
-for index in range(0, linhas, 1):
-    print(tarde[index]);
-
-print("Mapa da reserva da casa de show NOITE: ")
-for index in range(0, linhas, 1):
-    print(noite[index]);
+    else:
+        retornar = input("Deseja realizar mais alguma reserva? (S/N)")
+ 
+print("Mapa das reservas do show separados por período:")
+ 
+print("Reservas da Manhã")
+for i in range(0, fileiras, 1):
+    print(manha[i])
+ 
+print("Reservas da Tarde")
+for i in range(0, fileiras, 1):
+    print(tarde[i])
+ 
+print("Reservas da Noite")
+for i in range(0, fileiras, 1):
+    print(noite[i])
