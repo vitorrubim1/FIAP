7558b59eaef5396b760588b0035f77e59a8b9a40
Vitor Rubim <48107882+vitorrubim1@users.noreply.github.com>
2023-02-14 22:13:20 -0300

    add api request with python

---
+++ joseffe-barroso/Python/aula08/exercise02.py
@@ -0,0 +1,14 @@
+import requests
+
+user = input("Digite o usuário do github: ")
+url = f" https://api.github.com/users/{user}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    dados = response.json()
+    print(f"Nome: {dados['name']}")
+    print(f"Seguidores: {dados['followers']}")
+    print(f"Quantiade de repositórios: {dados['public_repos']}")
+else:
+    print("Usuário não encontrado.")
