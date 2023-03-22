ef4387fd23de53aff441e5fc9707bb41fe8bbf93
Vitor Rubim <48107882+vitorrubim1@users.noreply.github.com>
2023-03-22 20:20:39 -0300

    Update Aula02.sql

--- marcel-thome/Aula02.sql
+++ marcel-thome/Aula02.sql
@@ -4,19 +4,36 @@ create table categoria(
     Descricao varchar(50) constraint categoria_Descricao_nn not null
 );
 
-insert into categoria values (1, 'Comédia');
-insert into categoria values (2, 'Drama');
-insert into categoria values (3, 'Aventura');
-insert into categoria values (4, 'Terror');
-select * from categoria;
-
------------- RELACIONAMENTO (ATOR - FILME) ------------ 
-create table estrela(
-    fk_ator number(4) constraint ator_fk references ator,
-    fk_filme number(4) constraint filme_fk references filme 
+------------ FILME ------------ 
+create table filme(
+    CodFilme number(4) constraint filme_CodFilme_pk primary key,
+    Titulo varchar(70) constraint filme_Titulo_nn not null,
+    Codcateg number(4) constraint filme_Codcateg_fk references categoria
 );
 
-drop table estrela;
+------------ Cliente ------------ 
+create table cliente(
+    CodCli number(4) constraint cliente_CodCli_pk primary key,
+    Prenome varchar(50) constraint cliente_Prenome_nn not null,
+    Sobrenome varchar(50),
+    Endereco varchar(70),
+    Telefone varchar(20)
+);
+
+------------ DVD ------------ 
+create table dvd(
+    NumDVD number(4) constraint dvd_NumDVD_pk primary key,
+    Tipo char(1),
+    fk_filme number(4) constraint dvd_filme_fk references filme
+);
+
+------------ RELACIONAMENTO (CLIENTE - DVD  ) ------------ 
+create table aluga(
+    CodCli number(4) constraint aluga_CodCli_cliente_fk references cliente,
+    NumDVD number(4) constraint aluga_NumDVD_dvd_fk references dvd,
+    DataRet date,
+    DataDev date
+);
 
 ------------ ATOR ------------ 
 create table ator(
@@ -26,12 +43,18 @@ create table ator(
     Datanasc date
 );
 
------------- FILME ------------ 
-create table filme(
-    CodFilme number(4) constraint filme_CodFilme_pk primary key,
-    Titulo varchar(70) constraint filme_Titulo_nn not null,
-    fk_categoria number(4) constraint filme_categoria_fk references categoria
+------------ RELACIONAMENTO (ATOR - FILME) ------------ 
+create table estrela(
+    Codator number(4) constraint estrela_Codator_fk references ator,
+    CodFilme number(4) constraint estrela_CodFilme_fk references filme 
 );
+
+insert into categoria values (1, 'Comédia');
+insert into categoria values (2, 'Drama');
+insert into categoria values (3, 'Aventura');
+insert into categoria values (4, 'Terror');
+
+select * from categoria;
 
 insert into filme values (1, 'Sai pra lá', 1);
 insert into filme values (2, 'Ajuda Eterna', 2);
@@ -44,23 +67,6 @@ insert into filme values (8, 'A melodia da vida', 1);
 
 select * from filme;
 
------------- RELACIONAMENTO (CLIENTE - DVD  ) ------------ 
-create table aluga(
-    fk_cliente number(4) constraint cliente_fk references cliente,
-    fk_dvd number(4) constraint dvd_fk references dvd,
-    DataRet date,
-    DataDev date
-);
-
------------- Cliente ------------ 
-create table cliente(
-    CodCli number(4) constraint cliente_CodCli_pk primary key,
-    Prenome varchar(50) constraint cliente_Prenome_nn not null,
-    Sobrenome varchar(50),
-    Endereco varchar(70),
-    Telefone varchar(20)
-);
-
 insert into cliente values (1, 'João', 'Silva', 'Rua da Cruz sem P�', '4444-1111');
 insert into cliente values (2, 'Antônio', 'Ferreira', 'Av. da Vila Velha', '6660-9333');
 insert into cliente values (3, 'Fabio', 'Dias', 'Rua Antonio Vieira', '2337-0393');
@@ -68,13 +74,6 @@ insert into cliente values (4, 'Andreia', 'Melo', 'Rua da Praia Bonita', '8989-7
 insert into cliente values (5, 'Murilo ', 'Fontes', 'Av. dos Autonomistas', '9090-9090');
 
 select * from cliente;
-
------------- DVD ------------ 
-create table dvd(
-    NumDVD number(4) constraint dvd_NumDVD_pk primary key,
-    Tipo char(1),
-    fk_filme number(4) constraint dvd_filme_fk references filme
-);
 
 insert into dvd values (1, 'S', 1);
 insert into dvd values (2, 'S', 1);
